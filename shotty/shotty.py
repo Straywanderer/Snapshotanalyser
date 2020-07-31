32f3b59de6827597a27760ae2b1226e5545a26f2
Straywanderer <tobyfinnemore@gmail.com>
2020-07-31 15:57:38 +0100

    Updated script with error

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -5,17 +5,32 @@ session = boto3.Session(profile_name='shotty')
 ec2 = session.resource('ec2')
 
 @click.command()
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<name>)")
 def list_instances():
     "List EC2 instances"
+    intances = []
+
+    if project:
+        filters = [{'Name':'tag:Project', 'Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
     for i in ec2.instances.all():
+        tags = { t['Key']: t['Value'] for t in i.tags or [] }
         print(','.join((
             i.id,
             i.instance_type,
             i.placement['AvailabilityZone'],
             i.state['Name'],
-            i.public_dns_name)))
+            i.public_dns_name,
+            tags.get('Project', '<no project>')
+            )))
 
     return
 
 if __name__ == '__main__':
     list_instances()
+
+    ##something in lines 20-34 is going wrong, don't know what
